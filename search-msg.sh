9c2ed7bde9ff772eb4522f00c7a5fe8345744a7f
Rahix <rahix@rahix.de>
2018-05-02 16:14:13 +0200

    Add commandline scripts

---
+++ search-msg.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+SEARCH=$1
+
+RESULTS=$(jq "$(cat <<EOF
+[
+  [
+    .[
+      [
+        .[].text
+        | test("${SEARCH}"; "i")
+      ]
+      | indices(true)
+      | .[]
+     ]
+  ] as \$results
+  | \$results | map(true) | indices(true)
+  | .[] | {"index": ., "msg": (\$results[.].cls + ": " + \$results[.].text), "link": \$results[.].link}
+]
+EOF
+)" voicelines.json)
+
+echo $RESULTS | jq -r '.[] | "(" + (.index + 1 | tostring) + ") " + .msg' -
+echo -n "Choice: "
+read choice
+
+if [ ${choice} = "q" ]; then
+  exit
+fi
+
+echo Link: $(echo $RESULTS | jq -r ".[${choice} - 1].link" -)
